aaaa7b1c35f585e9c53a49e10cae812d0864a171
Nathan Dev <nathan@camposwebsolutions.com>
2020-03-27 02:14:54 -0300

    first project master

---
+++ web/src/pages/Login/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { FiLogIn } from 'react-icons/fi'
+
+import './styles.css';
+
+//importando imagem
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
+
+export default function Login() {
+  return (
+    <div className="login-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Heroes" />
+
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="Seu id" />
+          <button type="submit" className="button">Entrar</button>
+
+          <Link to="/register" className="back-link">
+            <FiLogIn size={16} color="#e02041" /> Não tenho cadastro
+          </Link>
+        </form>
+      </section>
+
+      <img src={heroesImg} alt="Heroes" />
+    </div>
+  )
+}
